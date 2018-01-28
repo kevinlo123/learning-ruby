e7ce004a051859d5f9a92bd06c6a166116162250
Kevin <lopezkevin175@gmail.com>
2018-01-28 04:09:30 -0500

    Cleaned up D.R.Y code on conditionals

--- rock-paper-scissors.rb
+++ rock-paper-scissors.rb
@@ -13,22 +13,14 @@ def start_rock_paper_scissors
     if !(options.include?(users_choice))
       puts "This is not a valid option"
       next
-    elsif users_choice == computers_choice
+    elsif users_choice == computers_choice 
       puts "There was a tie!" + " your choice: " + users_choice + "," + " computers choice: " + computers_choice
       puts "Your score: " + users_score.to_s
       puts "Computers score: " + computers_score.to_s
       next
-    elsif users_choice == "rock" && computers_choice == "scissors"
-      users_score += 1
-      puts "You win!" + " your choice: " + users_choice + "," + " computers choice: " + computers_choice
-      puts "Your score: " + users_score.to_s
-      puts "Computers score: " + computers_score.to_s
-    elsif users_choice == "paper" && computers_choice == "rock"
-      users_score += 1
-      puts "You win!" + " your choice: " + users_choice + "," + " computers choice: " + computers_choice
-      puts "Your score: " + users_score.to_s
-      puts "Computers score: " + computers_score.to_s
-    elsif users_choice == "scissors" && computers_choice == "paper"
+    elsif users_choice == "rock" && computers_choice == "scissors" ||
+          users_choice == "paper" && computers_choice == "rock" ||
+          users_choice == "scissors" && computers_choice == "paper"
       users_score += 1
       puts "You win!" + " your choice: " + users_choice + "," + " computers choice: " + computers_choice
       puts "Your score: " + users_score.to_s
